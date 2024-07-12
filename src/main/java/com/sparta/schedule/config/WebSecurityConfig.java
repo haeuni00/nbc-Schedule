7733d55a25134511af333faf715ff160187b1566
haeuni00 <leejw6311@naver.com>
2024-07-12 22:24:11 +0900

    git

--- src/main/java/com/sparta/schedule/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/schedule/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+//
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
 @EnableGlobalMethodSecurity(securedEnabled = true)
